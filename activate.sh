890ac45bc98d4aeea87a01ab860294166a8a5ba9
Frank Massi <frankmassi@gmail.com>
2017-09-05 16:34:31 +0000

    fix file-linking, add vim/tmux navigation

--- activate.sh
+++ activate.sh
@@ -2,21 +2,37 @@
 
 set -e
 
-# link my files
-dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
-dotfiles=(psqlrc tmux.conf vimrc.bundles.local vimrc_local vimrc_local_functions)
-for name in $dotfiles; do
-  rm -rf "${HOME}/.${name}"
-  ln -s "${dotfiles_dir}/${name}" "${HOME}/.${name}"
-done
+cmd_step() {
+  echo "=============================================="
+  echo $1
+  shift
+  $@
+  echo "=============================================="
+  echo
+}
+
+
+
+link_files() {
+  dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
+  dotfiles=(psqlrc tmux.conf vimrc.bundles.local vimrc_local vimrc_local_functions)
+  for name in ${dotfiles[@]}; do
+    echo "  - ${name}"
+    rm -rf "${HOME}/.${name}"
+    ln -s "${dotfiles_dir}/${name}" "${HOME}/.${name}"
+  done
+}
+
+cmd_step "linking_dotfiles" link_files
 
 # Clone base dotfiles from braintree
 if [ ! -d vim_dotfiles ]; then
-  git clone https://github.com/braintreeps/vim_dotfiles
+  cmd_step "cloning braintreeps/vim_dotfiles" git clone https://github.com/braintreeps/vim_dotfiles
 fi
 
 # activate everything
-cd vim_dotfiles
-git pull origin master
-bash activate.sh
-cd ..
+cmd_step "updating braintreeps/vim_dotfiles" cd vim_dotfiles && git pull origin master
+
+cmd_step "activating all the things" bash activate.sh && cd ..
+
+echo "Success!"
