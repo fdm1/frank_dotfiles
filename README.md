3de797d6982dbf608320ed22460408e2b49c51d2
Frank Massi <frankmassi@gmail.com>
2017-09-07 20:21:45 +0000

    dynamic file finding, tmux multiversion control

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 This contains extra dotfiles, including 'local' vimrc/vim plugins.
 The vim additions are meant to be loaded along side https://github.com/braintreeps/vim_dotfiles
 
-To install:
+
+Install
+-------
 
 ```
-$ bash activate.sh
+$ git clone https://github.com/fdm1/frank_dotfiles
+$ ./frank_dotfiles/activate.sh
 ```
 
 This will symlink all personal files to the root directory, clone braintreeps/vim_dotfiles (if needed), pull that repo,
 and activate/install everything.
 
+Requirements / Setup
+--------------------
+
+Requires:
+* git
+* sudo
+
+This will install/update the following packages (if needed):
+* vim (upgrade to 8.0+)
+* tmux
+* wget
+* curl
--- activate.sh
+++ activate.sh
@@ -11,17 +11,92 @@ cmd_step() {
   echo
 }
 
+get_vim_8() {
+  sudo apt-get install -y \
+    software-properties-common \
+    python-software-properties
 
+  sudo add-apt-repository -y ppa:jonathonf/vim
+  sudo apt-get update
+  sudo apt-get install -y vim
+}
+
+needs_dependencies() {
+  needs_deps=false
+  packages=(tmux wget curl)
+  for package in ${packages[@]}; do
+    if [ ! $(which ${package}) ]; then
+      needs_deps=true
+    else
+      echo "${package} exists"
+    fi
+  done
+
+  if [ $(which vim) ]; then
+    VIMVERSION=$(vim --version | head -1 | cut -d ' ' -f 5)
+    if [[ ${VIMVERSION} < 8 ]]; then 
+      needs_deps=true
+    else
+      echo "VIM 8+ present"
+    fi
+  else
+    needs_deps=true
+  fi
+  echo ${needs_deps}
+}
+
+get_dependencies() {
+  sudo apt-get update
+  packages=(tmux wget curl)
+
+  for package in ${packages[@]}; do
+    if [ ! $(which ${package}) ]; then
+      sudo apt-get install -y ${package}
+    fi
+  done
+
+  if [ $(which vim) ]; then
+    VIMVERSION=$(vim --version | head -1 | cut -d ' ' -f 5)
+    if [[ ${VIMVERSION} < 8 ]]; then 
+      get_vim_8
+    fi
+  else
+    get_vim_8
+  fi
+}
+
+tmux_file() {
+  if [[ $(tmux -V | awk -F ' ' '{print $NF}') < 2 ]]; then
+    echo tmux_lt_2.conf
+  else
+    echo tmux_gte_2.conf
+  fi
+}
 
 link_files() {
-  dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
-  dotfiles=(psqlrc tmux.conf vimrc.bundles.local vimrc_local vimrc_local_functions)
+  dotfiles_dir=$(cd "$(dirname "$0")"; pwd)/dotfiles
+  dotfiles=$(ls -1 ${dotfiles_dir} | grep -v tmux)
+  dotfiles=$(echo "${dotfiles} $(tmux_file)")
+
   for name in ${dotfiles[@]}; do
-    echo "  - ${name}"
-    rm -rf "${HOME}/.${name}"
-    ln -s "${dotfiles_dir}/${name}" "${HOME}/.${name}"
+
+    if [[ ${name} == tmux* ]]; then
+      home_name="${HOME}/.tmux.conf"
+    else
+      home_name="${HOME}/.${name}"
+    fi
+
+    echo "  - ${dotfiles_dir}/${name} => ${home_name}"
+    rm -rf "${home_name}"
+    ln -s "${dotfiles_dir}/${name}" "${home_name}"
   done
 }
+
+if [[ $(echo $(needs_dependencies)) == "true" ]]; then
+  cmd_step "installing dependencies" get_dependencies
+else
+  echo "all dependencies exist"
+fi
 
 cmd_step "linking_dotfiles" link_files
 
@@ -30,9 +105,12 @@ if [ ! -d vim_dotfiles ]; then
   cmd_step "cloning braintreeps/vim_dotfiles" git clone https://github.com/braintreeps/vim_dotfiles
 fi
 
-# activate everything
 cmd_step "updating braintreeps/vim_dotfiles" cd vim_dotfiles && git pull origin master
+cmd_step "activating all the things" bash activate.sh
 
-cmd_step "activating all the things" bash activate.sh && cd .. && tmux source-file ~/.tmux.conf
+if [[ $(ps aux | grep tmux | grep -v grep) ]]; then
+  cmd_step "reloading tmux.conf"
+  tmux source ${HOME}/.tmux.conf > /dev/null 2>&1
+fi
 
 echo "Success!"
